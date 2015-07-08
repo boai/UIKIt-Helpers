ae6d7e145b68df973704af1c63956c9020fedc2d
Kain <evgeniy@macsoftex.com>
2015-07-08 11:50:16 +0300

    BlockAlerts memory leak fix in ios 8

--- BlocksAlerts/BlockAlertView.h
+++ BlocksAlerts/BlockAlertView.h
@@ -8,20 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BlockAlertView : NSObject <UIAlertViewDelegate,UITextFieldDelegate>
+typedef NS_ENUM(NSInteger, BlockAlertViewStyle) {
+    BlockAlertViewStyleDefault = 0,
+    BlockAlertViewStyleSecureTextInput,
+    BlockAlertViewStylePlainTextInput,
+    BlockAlertViewStyleLoginAndPasswordInput
+};
 
--(id)initWithTitle:(NSString*)title message:(NSString*)message;
+@interface BlockAlertView : NSObject 
+
+-(instancetype)initWithTitle:(NSString*)title message:(NSString*)message;
+-(instancetype)initWithTitle:(NSString*)title message:(NSString*)message style:(BlockAlertViewStyle)style;
 -(void)addButton:(NSString*)title withBlock:(void (^)(BlockAlertView* alert))block;
+-(void)addCancelButton:(NSString*)title withBlock:(void (^)(BlockAlertView* alert))block;
 
 -(void)show;
 
 -(UITextField*)textFieldAtIndex:(NSInteger)index;
 
-@property (nonatomic) UIAlertViewStyle style;
-@property (nonatomic) NSInteger cancelButtonIndex;
-@property (nonatomic) NSInteger returnButton;
 @property (nonatomic, readonly) NSInteger numberOfButtons;
 
 @property (nonatomic,copy) void (^alertDidShowBlock)(BlockAlertView *alert);
+@property (nonatomic,copy) BOOL (^alertShouldDismissOnTextFieldReturnWithCompletionBlock)(BlockAlertView *alert,UITextField *textField);
 
 @end
--- BlocksAlerts/BlockAlertView.m
+++ BlocksAlerts/BlockAlertView.m
@@ -8,6 +8,15 @@
 
 #import "BlockAlertView.h"
 
+typedef enum : NSUInteger {
+    BlockAlertViewButtonTypeDefault,
+    BlockAlertViewButtonTypeCancel,
+} BlockAlertViewButtonType;
+
+@interface BlockAlertView () <UIAlertViewDelegate,UITextFieldDelegate>
+
+@end
+
 @implementation BlockAlertView
 {
     UIAlertView *_alertView;
@@ -15,34 +24,143 @@
     NSMutableArray *buttonsBlocks;
     
     id context;
+    
+    UIAlertController *_alertController;
+    BlockAlertViewStyle _style;
 }
 
--(id)initWithTitle:(NSString*)title message:(NSString*)message
+#pragma mark - IOS 8
+
+-(instancetype)initIOS8WithTitle:(NSString *)title message:(NSString *)message style:(BlockAlertViewStyle)style
+{
+    if (self = [super init])
+    {
+        _alertController = [UIAlertController alertControllerWithTitle:title
+                                                               message:message
+                                                        preferredStyle:UIAlertControllerStyleAlert];
+        
+        switch (style) {
+            case BlockAlertViewStyleLoginAndPasswordInput:
+                [self addLoginAndPaswordFields];
+                break;
+                
+            case BlockAlertViewStylePlainTextInput:
+                [self addPlainTextInput];
+                break;
+                
+            case BlockAlertViewStyleSecureTextInput:
+                [self addSecureTextInput];
+                break;
+            default:
+                break;
+        }
+    }
+    
+    return self;
+}
+
+-(void)addLoginAndPaswordFields
+{
+    typeof(self) __weak weakSelf = self;
+    [_alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.delegate = weakSelf;
+    }];
+    
+    [_alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.secureTextEntry = YES;
+        textField.delegate = weakSelf;
+    }];
+}
+
+-(void)addPlainTextInput
+{
+    typeof(self) __weak weakSelf = self;
+    [_alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.delegate = weakSelf;
+    }];
+}
+
+-(void)addSecureTextInput
+{
+    typeof(self) __weak weakSelf = self;
+    [_alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.secureTextEntry = YES;
+        textField.delegate = weakSelf;
+    }];
+}
+
+-(instancetype)initWithTitle:(NSString *)title message:(NSString *)message
+{
+    return [self initIOS8WithTitle:title message:message style:BlockAlertViewStyleDefault];
+}
+
+-(instancetype)initWithTitle:(NSString*)title message:(NSString*)message style:(BlockAlertViewStyle)style
 {
     if (self = [self init])
     {
-        _alertView = [[UIAlertView alloc] initWithTitle:title
-                                                message:message
-                                               delegate:self
-                                      cancelButtonTitle:nil
-                                      otherButtonTitles:nil];
-        
-        buttonsBlocks = [NSMutableArray array];
-        
-        self.returnButton = -1;
+        _style = style;
+        if ([[UIDevice currentDevice].systemVersion floatValue] < 8.0)
+        {
+            _alertView = [[UIAlertView alloc] initWithTitle:title
+                                                    message:message
+                                                   delegate:self
+                                          cancelButtonTitle:nil
+                                          otherButtonTitles:nil];
+            
+            buttonsBlocks = [NSMutableArray array];
+            _alertView.alertViewStyle = (UIAlertViewStyle)style;
+        }
+        else
+        {
+            self = [self initIOS8WithTitle:title message:message style:style];
+        }
     }
     return self;
 }
 
 -(void)addButton:(NSString*)title withBlock:(void (^)(BlockAlertView*))block
 {
-    [_alertView addButtonWithTitle:title];
-    if (block)
-        [buttonsBlocks addObject:[block copy]];
+    [self addButton:title buttonType:BlockAlertViewButtonTypeDefault withBlock:block];
+}
+
+-(void)addCancelButton:(NSString *)title withBlock:(void (^)(BlockAlertView *))block
+{
+    [self addButton:title buttonType:BlockAlertViewButtonTypeCancel withBlock:block];
+}
+
+-(void)addButton:(NSString *)title buttonType:(BlockAlertViewButtonType)type withBlock:(void (^)(BlockAlertView *))block
+{
+    if ([[UIDevice currentDevice].systemVersion floatValue] < 8.0)
+    {
+        NSInteger indx = [_alertView addButtonWithTitle:title];
+        
+        if (type == BlockAlertViewButtonTypeCancel)
+            _alertView.cancelButtonIndex = indx;
+        
+        if (block)
+            [buttonsBlocks addObject:[block copy]];
+        else
+        {
+            void(^emptyBlock)(BlockAlertView*) = ^(BlockAlertView* alert) {};
+            [buttonsBlocks addObject:[emptyBlock copy]];
+        }
+    }
     else
     {
-        void(^emptyBlock)(BlockAlertView*) = ^(BlockAlertView* alert) {};
-        [buttonsBlocks addObject:[emptyBlock copy]];
+        typeof(self) __weak weakSelf = self;
+        
+        UIAlertActionStyle style = UIAlertActionStyleDefault;
+        if (type == BlockAlertViewButtonTypeCancel)
+        {
+            style = UIAlertActionStyleCancel;
+        }
+        
+        UIAlertAction *action = [UIAlertAction actionWithTitle:title style:style handler:^(UIAlertAction *action) {
+            if (block)
+                block(weakSelf);
+            [weakSelf dissmissed];
+        }];
+        [_alertController addAction:action];
     }
 }
 
@@ -50,24 +168,72 @@
 {
     context = self;
     
-    if (_alertView.alertViewStyle!=UIAlertViewStyleDefault && _alertView.alertViewStyle!=UIAlertViewStylePlainTextInput)
+    if ([[UIDevice currentDevice].systemVersion floatValue] < 8.0)
     {
-        [_alertView textFieldAtIndex:0].delegate = self;
-        [_alertView textFieldAtIndex:1].delegate = self;
+        if (_alertView.alertViewStyle!=UIAlertViewStyleDefault && _alertView.alertViewStyle!=UIAlertViewStylePlainTextInput)
+        {
+            [_alertView textFieldAtIndex:0].delegate = self;
+            [_alertView textFieldAtIndex:1].delegate = self;
+        }
+        else if (_alertView.alertViewStyle == UIAlertViewStylePlainTextInput)
+        {
+            [_alertView textFieldAtIndex:0].delegate = self;
+        }
+        
+        [_alertView show];
     }
-    else if (_alertView.alertViewStyle == UIAlertViewStylePlainTextInput)
+    else
     {
-        [_alertView textFieldAtIndex:0].delegate = self;
+        [self present];
+    }
+}
+
+- (UIViewController *) getRootController
+{
+    NSMutableArray *windows = [[UIApplication sharedApplication].windows mutableCopy];
+    
+    UIWindow *window = windows.lastObject;
+    UIViewController *rootController = nil;
+    
+    while (window && rootController == nil) {
+        rootController = window.rootViewController;
+        if (rootController == nil)
+        {
+            [windows removeLastObject];
+            window = windows.lastObject;
+        }
     }
     
-    [_alertView show];
+    return rootController;
+}
+
+-(void)present
+{
+    UIViewController *rootController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    if (rootController == nil)
+    {
+        rootController = [self getRootController];
+    }
+    
+    [rootController presentViewController:_alertController
+                                 animated:YES
+                               completion:^{
+                                   [self didPresent];
+                               }];
 }
 
 -(void)dissmissed
 {
+    if ([[UIDevice currentDevice].systemVersion floatValue] < 8.0)
+    {
+        _alertView = nil;
+    }
+    else
+    {
+        _alertController = nil;
+    }
     dispatch_async(dispatch_get_main_queue(), ^{
         context = nil;
-        _alertView = nil;
     });
 }
 
@@ -81,34 +247,28 @@
 
 #pragma mark - Properties
 
--(void)setCancelButtonIndex:(NSInteger)cancelButtonIndex
-{
-    _alertView.cancelButtonIndex = cancelButtonIndex;
-}
-
--(NSInteger)cancelButtonIndex
-{
-    return _alertView.cancelButtonIndex;
-}
-
 -(UITextField*)textFieldAtIndex:(NSInteger)index
 {
-    return [_alertView textFieldAtIndex:index];
-}
-
--(void)setStyle:(UIAlertViewStyle)style
-{
-    _alertView.alertViewStyle = style;
-}
-
--(UIAlertViewStyle)style
-{
-    return _alertView.alertViewStyle;
+    if ([[UIDevice currentDevice].systemVersion floatValue] < 8.0)
+    {
+        return [_alertView textFieldAtIndex:index];
+    }
+    else
+    {
+        return _alertController.textFields[index];
+    }
 }
 
 -(NSInteger)numberOfButtons
 {
-    return _alertView.numberOfButtons;
+    if ([[UIDevice currentDevice].systemVersion floatValue] < 8.0)
+    {
+        return _alertView.numberOfButtons;
+    }
+    else
+    {
+        return _alertController.actions.count;
+    }
 }
 
 #pragma mark - UIAlertViewDelegate
@@ -120,6 +280,11 @@
 
 -(void)didPresentAlertView:(UIAlertView *)alertView
 {
+    [self didPresent];
+}
+
+-(void)didPresent
+{
     if (self.alertDidShowBlock)
     {
         self.alertDidShowBlock(self);
@@ -130,18 +295,31 @@
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    if (textField == [_alertView textFieldAtIndex:0])
+    if (textField == [self textFieldAtIndex:0])
     {
-        if (_alertView.alertViewStyle==UIAlertViewStyleLoginAndPasswordInput)
+        if (_style == BlockAlertViewStyleLoginAndPasswordInput)
         {
-            [[_alertView textFieldAtIndex:1] becomeFirstResponder];
+            [[self textFieldAtIndex:1] becomeFirstResponder];
             return YES;
         }
     }
     
-    if (self.returnButton<0) return NO;
-    
-    [_alertView dismissWithClickedButtonIndex:self.returnButton animated:YES];
+    if (self.alertShouldDismissOnTextFieldReturnWithCompletionBlock)
+    {
+        BOOL shouldDissmiss = self.alertShouldDismissOnTextFieldReturnWithCompletionBlock(self,textField);
+        
+        if (shouldDissmiss)
+        {
+            if ([[UIDevice currentDevice].systemVersion floatValue] < 8.0)
+            {
+                [_alertView dismissWithClickedButtonIndex:-10 animated:YES];
+            }
+            else
+            {
+                [_alertController dismissViewControllerAnimated:YES completion:NO];
+            }
+        }
+    }
     
     return YES;
 }
